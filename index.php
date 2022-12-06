e942f9b17e8d672d34d820ff2c5c769ea9203ca7
Tom <tom.slaytor@gmail.com>
2022-12-06 10:51:51 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- including bootstrap below -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    Skate Shop
+                </a>
+                <ul>
+                    <li class="nav-item">
+                        <a class="nav-link">Skateboards</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">Shoes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">Clothes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">Accessories</a>
+                    </li>
+                </ul>
+                
+            </div>
+        </nav>
+
+    </body>
+</html>
+
